d90140a4602649ad84f44851feb1f84e3cb2cbd7
hoangnv34 <gadien102@gmail.com>
2025-08-01 08:10:22 +0700

    update traction and store

--- AutoPartData.sql
+++ AutoPartData.sql
@@ -1,4 +1,4 @@
-use AutoParts
+﻿use AutoParts
 
 --CREATE TABLE product_category (
 --    id SMALLINT PRIMARY KEY,
@@ -116,7 +116,154 @@ use AutoParts
 
 --select * from GetValueCount
 --start index substring = 1
-select *, SUBSTRING(v.manufacturer_name, 1 ,3) as 'sub'
-from compatibility c
-join vehicles v on c.vehicles_id = v.id
-where vehicle_type_id = 1
+--select *, SUBSTRING(v.manufacturer_name, 1 ,3) as 'sub'
+--from compatibility c
+--join vehicles v on c.vehicles_id = v.id
+--where vehicle_type_id = 1
+
+--create procedure GetSellerTop 
+--	@sellerID nvarchar(50)
+--as
+--Begin
+--	select top 1 COUNT(a.seller_id) as CountData,s.seller_name, CAST(s.seller_name AS NVARCHAR(255)) AS seller_name
+--	from applications a
+--	join seller s on a.seller_id = s.id
+--	group by a.seller_id, s.seller_name, CAST(s.seller_name AS NVARCHAR(255))
+--	order by CountData desc
+--end
+
+-- Call store
+--exec GetSellerTop @sellerID = '1'
+
+--Remove Store
+--drop procedure GetSellerTop
+
+
+BEGIN TRANSACTION;
+
+UPDATE seller
+SET address = 'Blocked'
+WHERE id = 79;
+
+Begin TRANSACTION
+	Begin try 
+		UPDATE [dbo].[seller]
+		   SET 
+			  [address] = 'HaNoi'
+			  ,[mobile_number] = '012556344'
+		 WHERE id = 79;
+
+		 INSERT INTO [dbo].[applications]
+		    ([app_id]
+           ,[headline]
+           ,[price_gel]
+           ,[price_usd]
+           ,[app_register_date]
+           ,[status_id]
+           ,[category_id]
+           ,[vehicle_type_id]
+           ,[seller_id]
+           ,[item_condition]
+           ,[insert_date])
+     VALUES
+           (3
+           ,'Hello'
+           ,1
+           ,20
+           ,'2025/03/14'
+           ,1
+           ,20
+           ,1
+           ,79
+           ,'Test'
+           ,'2025/03/04');
+		 COMMIT TRANSACTION;
+	end try
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+
+		PRINT ERROR_MESSAGE();
+	END CATCH;
+
+--create function fn_GetFullName(@first nvarchar(50), @last nvarchar(50))
+--returns nvarchar(50)
+--as 
+--	begin
+--		return @last + '' + @first
+--	end;
+
+-- Call Function Scalar Function
+--SELECT dbo.fn_GetFullName('Anh', 'Nguyen') AS FullName;
+
+-- create Table-Valued Function (trả về bảng)
+--create function GetAllicationByID(@application_id int)
+--returns table
+--return (
+--	select * from applications
+--		where app_id = @application_id
+--);
+
+-- Call Function Table-Valued
+--select * from  GetAllicationByID(1)
+
+-- check sesstion block 
+--SELECT 
+--    r.session_id,
+--    r.blocking_session_id,
+--    r.status,
+--    r.wait_type,
+--    r.wait_time,
+--    r.cpu_time,
+--    r.total_elapsed_time,
+--    t.text AS running_sql_text,
+--    qp.query_plan
+--FROM sys.dm_exec_requests r
+--JOIN sys.dm_exec_sessions s ON r.session_id = s.session_id
+--CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) AS t
+--OUTER APPLY sys.dm_exec_query_plan(r.plan_handle) AS qp
+--WHERE r.blocking_session_id <> 0;
+
+----Cound Block
+--SELECT 
+--    t.text AS [SQL Text],
+--    COUNT(*) AS [BlockedCount]
+--FROM sys.dm_exec_requests r
+--JOIN sys.dm_exec_sessions s ON r.session_id = s.session_id
+--CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) AS t
+--WHERE r.blocking_session_id <> 0
+--GROUP BY t.text
+--ORDER BY [BlockedCount] DESC;
+
+
+
+-- tach ra lam 2 tab
+--BEGIN TRANSACTION;
+--BEGIN TRY 
+--    UPDATE [dbo].[seller]
+--       SET 
+--          [address] = 'HaNoi',
+--          [mobile_number] = '012556344'
+--     WHERE id = 79; 
+
+--    INSERT INTO [dbo].[applications]
+--        ([app_id], [headline], [price_gel], [price_usd],
+--         [app_register_date], [status_id], [category_id], 
+--         [vehicle_type_id], [seller_id], [item_condition], [insert_date])
+--    VALUES
+--        (3, 'Hello', 1, 20, '2025-03-14', 1, 20, 1, 79, 'Test', '2025-03-04');
+
+--    COMMIT TRANSACTION;
+--END TRY
+--BEGIN CATCH
+--    ROLLBACK TRANSACTION;
+--    PRINT ERROR_MESSAGE();
+--END CATCH;
+
+---- tab 2
+
+--BEGIN TRANSACTION;
+
+--UPDATE seller
+--SET address = 'Blocked'
+--WHERE id = 79;
+--COMMIT
