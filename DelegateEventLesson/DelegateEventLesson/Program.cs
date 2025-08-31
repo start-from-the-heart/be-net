d4bb78c0ca6374d0244e0f660dd9c080590fd147
hoangnv34 <gadien102@gmail.com>
2025-08-31 23:14:20 +0700

    add event lesson

--- DelegateEventLesson/DelegateEventLesson/Program.cs
+++ DelegateEventLesson/DelegateEventLesson/Program.cs
@@ -62,6 +62,73 @@
 
             add<string> con = Concat;
             Console.WriteLine(Concat("Hello ", "World!!"));
+
+            // Function Delegate
+            Func<int, int, int> SumOffTowValue = Sum;
+
+            Console.WriteLine("Function Delegate: " + SumOffTowValue(10, 10));
+
+            // Function no Paramter
+            Func<int> ShowNumber = AsTheResult;
+
+            Console.WriteLine("Function Delegate No Paramter: " + ShowNumber());
+
+            // Anonymous Method
+
+            Func<int, int> showNumberOfData = delegate (int number)
+            {
+                return number;
+            };
+
+            Console.WriteLine("Function Delegate with anoymous method: " + showNumberOfData(3));
+
+            // Function with lamda
+
+            Func<int> getRandomNumber = () => new Random().Next(1, 100);
+
+            Func<int, int, int> SumTwoValue = (x, y) => x + y;
+
+            Console.WriteLine("Function Delegate with lamda: " + getRandomNumber());
+
+            // Great than max 16 value in funcion delegate
+
+            // Action DeleteGate is not return
+
+            static void ConsolePrint(int i)
+            {
+                Console.WriteLine("Action Delegate: " + i);
+            }
+
+            Action<int> action = ConsolePrint;
+
+            action(1);
+
+            // Predict Delegate get 1 value input and return value boolean in required
+
+            static bool IsUpperCase(string str)
+            {
+                return str.Equals(str.ToUpper());
+            }
+
+            Predicate<string> isUpperCase = IsUpperCase;
+
+            Console.WriteLine(isUpperCase("VALUE"));
+
+            // anonymous method
+            //It cannot contain jump statement like goto, break or continue.
+            //It cannot access ref or out parameter of an outer method.
+            //It cannot have or access unsafe code.
+            //It cannot be used on the left side of the is operator.
+
+            //ProcessBusinessLogic bl = new ProcessBusinessLogic();
+            //bl.ProcessCompleted += bl_ProcessCompleted; // register with an event
+            //bl.StartProcess();
+        }
+
+        // event handler
+        public static void bl_ProcessCompleted()
+        {
+            Console.WriteLine("Process Completed!");
         }
 
         //prints 0
@@ -103,6 +170,13 @@
         public static string Concat(string str1, string str2)
         {
             return str1 + str2;
+        }
+
+        public static int AsTheResult()
+        {
+            Console.WriteLine("1");
+
+            return -1;
         }
     }
 
